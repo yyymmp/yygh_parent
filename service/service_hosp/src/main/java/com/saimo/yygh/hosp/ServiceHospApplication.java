73e7905bea0b5f79b9962e45cd6aa08f6193947f
yyymmp <j2538474647@163.com>
2021-07-26 23:34:31 +0800

    service_hosp基础模块可运行

---
+++ service/service_hosp/src/main/java/com/saimo/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,18 @@
+package com.saimo.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author clearlove
+ * @ClassName ServiceHospApplication.java
+ * @Description
+ * @createTime 2021年07月26日 23:12:00
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
