72750c4d63d94cd32934c723bc8251e8b464292c
yyymmp <j2538474647@163.com>
2021-07-29 00:07:32 +0800

    分页查询完成

--- service/service_hosp/src/main/java/com/saimo/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/saimo/yygh/hosp/controller/HospitalSetController.java
@@ -1,12 +1,22 @@
 package com.saimo.yygh.hosp.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.saimo.yygh.common.result.Result;
 import com.saimo.yygh.hosp.service.HospitalSetService;
 import com.saimo.yygh.model.hosp.HospitalSet;
+import com.saimo.yygh.vo.hosp.HospitalSetQueryVo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,19 +25,43 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description
  * @createTime 2021年07月26日 23:49:00
  */
-@RestController("admin/hosp/hospitalSet")
+@RestController
+@RequestMapping("admin/hosp/hospitalSet")
+@Api(tags = "医院设置管理")
 public class HospitalSetController {
 
     @Autowired
     private HospitalSetService hospitalSetService;
 
-    @GetMapping("findAll")
-    public List<HospitalSet> findAllHospitalSet() {
-        return hospitalSetService.list();
+
+    @ApiOperation(value = "获取所有医院设置")
+    @GetMapping("/findAll")
+    public Result<List<HospitalSet>> findAllHospitalSet() {
+        return Result.ok(hospitalSetService.list());
     }
 
-    @DeleteMapping("{id}")
-    public boolean removeHospitalSet(@PathVariable Long id) {
-        return hospitalSetService.removeById(id);
+    @ApiOperation(value = "逻辑删除医院设置")
+    @DeleteMapping("/{id}")
+    public Result<Boolean> removeHospitalSet(@PathVariable Long id) {
+        return Result.ok(hospitalSetService.removeById(id));
+    }
+
+    @ApiOperation(value = "分页查询医院设置")
+    @PostMapping("/findPageHospitalSet/{current}/{limit}")
+    public Result findPageHospitalSet(@PathVariable long current
+            , @PathVariable long limit
+            , @RequestBody(required = false) HospitalSetQueryVo hospitalSetQueryVo) {
+        //分页条件
+        Page<HospitalSet> page = new Page<>(current, limit);
+        //查询条件
+        QueryWrapper<HospitalSet> queryWrapper = new QueryWrapper();
+        if (!StringUtils.isEmpty(hospitalSetQueryVo.getHosname())) {
+            queryWrapper.like("hosname", hospitalSetQueryVo.getHosname());
+        }
+        if (!StringUtils.isEmpty(hospitalSetQueryVo.getHoscode())) {
+            queryWrapper.eq("hoscode", hospitalSetQueryVo.getHoscode());
+        }
+        Page<HospitalSet> pageHospitalSet = hospitalSetService.page(page, queryWrapper);
+        return Result.ok(pageHospitalSet);
     }
 }
